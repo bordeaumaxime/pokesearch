311d943bd29942ea910f9c2cab572e189505ff3f
bordeaumaxime <bordeau.maxime@gmail.com>
2024-02-07 15:34:59 +0000

    Create business models

---
+++ PokeSearch/app/src/main/java/com/pokesearch/data/model/Pokemon.kt
@@ -0,0 +1,10 @@
+package com.pokesearch.data.model
+
+data class Pokemon(
+    val id: Int,
+    val name: String,
+    val heightDecimeters: Float,
+    val weightHectoGrams: Float,
+    val types: List<PokemonType>,
+    val imageUrl: String,
+)
---
+++ PokeSearch/app/src/main/java/com/pokesearch/data/model/PokemonType.kt
@@ -0,0 +1,3 @@
+package com.pokesearch.data.model
+
+data class PokemonType(val name: String)
