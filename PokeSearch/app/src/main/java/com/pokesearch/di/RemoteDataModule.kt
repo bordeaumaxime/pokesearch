83069fb74ce372511fad5b7c629f5bfa50392123
bordeaumaxime <bordeau.maxime@gmail.com>
2024-02-08 18:39:33 +0000

    Add Cache with Okhttp

--- PokeSearch/app/src/main/java/com/pokesearch/di/RemoteDataModule.kt
+++ PokeSearch/app/src/main/java/com/pokesearch/di/RemoteDataModule.kt
@@ -1,10 +1,14 @@
 package com.pokesearch.di
 
+import android.content.Context
 import com.pokesearch.data.remote.PokeApiService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Cache
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -14,10 +18,21 @@ import javax.inject.Singleton
 object RemoteDataModule {
 
     @Provides
+    fun provideOkHttpClient(@ApplicationContext appContext: Context): OkHttpClient {
+        val cacheMaxSize = 70L * 1000L * 1000L // 70MB
+        val cache = Cache(appContext.cacheDir, cacheMaxSize)
+        return OkHttpClient
+            .Builder()
+            .cache(cache)
+            .build()
+    }
+
+    @Provides
     @Singleton
-    fun provideRetrofit(): Retrofit {
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
         return Retrofit.Builder()
             .baseUrl(PokeApiService.BASE_URL)
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
